b7961d211be98abed5786cfca987fe06f25f0172
NoobOtaku-terminal <dj.dhakad9580@gmail.com>
2024-11-04 22:55:07 +0530

    login sign up updated

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../components_css/login.css'; // Import the CSS file
+import styles from '../components_css/login.module.css'; // Import CSS Module
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -14,7 +14,6 @@ const Login = () => {
         if (email === 'user@example.com' && password === 'password') {
             alert("Login successful!");
             localStorage.setItem('isAuthenticated', 'true');
-
             navigate('/myaccount');
         } else {
             alert("Invalid credentials!");
@@ -22,28 +21,27 @@ const Login = () => {
     };
 
     return (
-        <div className='Container'>
-            <form className="form">
-                <div className='form-container'>
-                <div className="header">Sign In</div>
-                <div className="inputs">
-                    <input placeholder="Email" className="input" type="text" />
-                    <input placeholder="Password" className="input" type="password" />
-                    <div className="checkbox-container">
-                        <label className="checkbox">
-                            <input type="checkbox" id="checkbox" />
-                        </label>
-                        <label htmlFor="checkbox" className="checkbox-text">Remember me</label>
+        <div className={styles.Container}>
+            <form className={styles.form}>
+                <div className={styles['form-container']}>
+                    <div className={styles['login-header']}>Sign In</div>
+                    <div className={styles.inputs}>
+                        <input placeholder="Email" className={styles.input} type="text" />
+                        <input placeholder="Password" className={styles.input} type="password" />
+                        <div className={styles['checkbox-container']}>
+                            <label className={styles.checkbox}>
+                                <input type="checkbox" id="checkbox" />
+                            </label>
+                            <label htmlFor="checkbox" className={styles['checkbox-text']}>Remember me</label>
+                        </div>
+                        <button onClick={handleLogin} className={styles['signin-btn']}>Submit</button>
+                        <a className={styles.forget} href="#">Forget password?</a>
+                        <p className={styles['signup-link']}>Don't have an account? <a href="/signup">Sign up</a></p>
                     </div>
-                    <button onClick={()=>handleLogin} className="signin-btn1">Submit</button>
-                    <a className="forget" href="#">Forget password?</a>
-                    <p className="signup-link">Don't have an account? <a href="/signup">Sign up</a></p>
-                </div>
                 </div>
             </form>
         </div>
-
-                );
+    );
 };
 
-                export default Login;
+export default Login;
